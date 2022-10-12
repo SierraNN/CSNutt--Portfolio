b1ad6e5da2eb69451f6e477800c86ccfbc534849
Sierra Nutt <sierra.nutt@outlook.com>
2022-10-12 16:53:31 -0600

    basic structure plus logo

---
+++ connor-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Body from '../src/pages/Body'
+
+function App() {
+  return (
+    <div>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ connor-portfolio/src/pages/Body.jsx
@@ -0,0 +1,12 @@
+import '../pages/body.css'
+import image from '../images/logo.png'
+function Bio() {
+
+    return (
+    <div className='card'>
+        <img src= {image} />
+    </div>
+    )
+}
+
+export default Bio
